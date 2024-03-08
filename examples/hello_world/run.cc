8c7b2cf61b9794b806de091685dc6739dd3db837
austinvhuang <austinh@alum.mit.edu>
2024-03-08 17:59:54 -0500

    add README, license to hello_world

--- examples/hello_world/run.cc
+++ examples/hello_world/run.cc
@@ -1,3 +1,18 @@
+// Copyright 2024 Google LLC
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <iostream>
 
 // copybara:import_next_line:gemma_cpp
@@ -43,7 +58,7 @@ int main(int argc, char** argv) {
       tokenize("Write a greeting to the world.", model.Tokenizer());
   size_t ntokens = tokens.size();
 
-  // Callback
+  // This callback function gets invoked everytime a token is generated
   auto stream_token = [&pos, &gen, &ntokens, tokenizer = model.Tokenizer()](
                           int token, float) {
     ++pos;
