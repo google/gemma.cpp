1308355ff021edf9bdf5aebce314543b74c65627
Bibek Bhattarai <bibek.bhattarai@intel.com>
2026-04-23 19:23:55 +0000

    fixing the copyright info

--- ops/brgemm-inl.h
+++ ops/brgemm-inl.h
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2026 DeepMind Technologies Limited.
 // SPDX-License-Identifier: Apache-2.0
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
